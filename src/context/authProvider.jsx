4726db363b0c2a373e93201d4e6e8698ea989013
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-07 14:22:50 +0800

    Add another state user, setUser. This state is use for handling the profileStatus and autologout

--- src/context/authProvider.jsx
+++ src/context/authProvider.jsx
@@ -1,12 +1,29 @@
-import { createContext, useState } from "react"
+import { createContext,  useState } from "react"
 
 const AuthContext = createContext({})
 
+//context that wrap the entire app
 export function AuthProvider({children}) {
   const [auth, setAuth] = useState({})
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+
+  //retrieve localstorage user if username state is still null and save it to username state
+  //just a helper if ever the user state doesnt get the localStorage user
+  // useEffect(() => {
+  //   try {
+  //     if (user === null || user === undefined) {
+  //       const item = localStorage.getItem("user")
+  //       if (item) {
+  //         setUser(JSON.parse(item))
+  //       } 
+  //     }
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }, [user])
 
   return (
-    <AuthContext.Provider value={{auth, setAuth}}>
+    <AuthContext.Provider value={{auth, setAuth, user, setUser}}>
       {children}
     </AuthContext.Provider>
   ) 
