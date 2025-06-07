c25c79d69862d021dd4acd509dd8028c98ab3be2
Bartue <[bartbatiancila28@gmail.com]>
2025-06-07 14:25:40 +0800

    Import the RouterProvider from react-router and the created router. Then replace the App component with RouterProvider and router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import "./index.css"
 import App from './App.jsx'
 import { AuthProvider } from './context/authProvider.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from '../utils/routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 )
