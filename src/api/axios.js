0cec1bbb5b07013b433487736e781257f371e341
Bartue <[bartbatiancila28@gmail.com]>
2025-06-07 14:20:52 +0800

    Add a axiosPrivate instance that will be use for protected routes or routes that required jwt

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 const BASE_URL = "http://localhost:3000"
 
+//for public route
 export default axios.create({
   baseURL: BASE_URL
 });
+
+//for setting up the jwt auth
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: {"Content-Type": "application/json"},
+  withCredentials: true
+})
