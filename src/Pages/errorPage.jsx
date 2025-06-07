00702f95f6a2dcf8f02ac59052f2e5c0cca236f4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-07 14:29:38 +0800

    Create an errorPage. If page not found this page will be rendered

---
+++ src/Pages/errorPage.jsx
@@ -0,0 +1,27 @@
+import { useRouteError } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  const navigate = useNavigate();
+
+  //redirect from the previous page
+  const goBack = () => navigate(-1)
+
+  return (
+   <section class="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
+    <div class="container flex flex-col items-center ">
+        <div class="flex flex-col gap-6 max-w-md text-center">
+            <h2 class="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
+                <span class="sr-only">{error.statusText || error.message}</span>404
+            </h2>
+            <p class="text-2xl md:text-3xl dark:text-gray-300">Sorry, we couldn't find this page.</p>
+            <button onClick={goBack} class="px-8 py-4 text-xl font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200 cursor-pointer">Back to home</button>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default ErrorPage;
