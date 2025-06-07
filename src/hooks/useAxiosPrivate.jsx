075eb8af2d111f01a6881aff01751a573956bcbb
Bartue <[bartbatiancila28@gmail.com]>
2025-06-07 14:45:13 +0800

    CReates a useAxiosPrivate custom hook. this custom hook is reponsible for having a request for backend API enpoint that requires a verification of jwt/Bearer token.

---
+++ src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,51 @@
+import { axiosPrivate } from "../api/axios";
+import useRefreshToken from "./useRefreshToken"; 
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+
+function useAxiosPrivate() {
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+
+    //set up the authorization Bearer TOKEN with initial accessToken
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      config => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+        }
+        return config;
+      }, (error) => Promise.reject(error)
+    );
+
+    /*if initial accessToken expires call the useRefresh hook the create new accessToken
+      then set it up to the authorization header Bearer TOKEN
+    */
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      response => response,
+      async (error) => {
+        const prevRequest = error?.config;
+
+        if (error?.response?.status === 403 && !prevRequest.sent) {
+          console.log("INTERCEPTER RESPONSE")
+          prevRequest.sent = true;
+          const newAccessToken = await refresh();
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(error)
+      }
+    );
+
+    
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept)
+      axiosPrivate.interceptors.response.eject(responseIntercept)
+    }
+  }, [auth, refresh])
+
+  return axiosPrivate;
+}
+
+export default useAxiosPrivate;
