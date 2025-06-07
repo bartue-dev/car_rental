2ef029edfb11a84454903f997235c0d621974a8a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-07 14:48:15 +0800

    CReated a routes.jsx file. This handles the routing of the App.

---
+++ utils/routes.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../src/App";
+import ErrorPage from "../src/Pages/errorPage";
+import Home from "../src/Pages/publicRoute/home";
+import Login from "../src/Pages/publicRoute/login";
+import Register from "../src/Pages/publicRoute/register";
+import Unauthorized from "../src/Pages/unauthorized";
+import RequiredAuth from "../src/Pages/requiredAuth";
+import Admin from "../src/Pages/protectedRoute/admin/admin";
+import User from "../src/Pages/protectedRoute/user/user";
+import PersistLogin from "../src/Pages/persistLogin";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      // Public routes
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+
+      // Private routes can be added here
+      { element: <PersistLogin />,
+        children: [
+        { element: <RequiredAuth allowedRole="ADMIN"/>,
+          children: [
+            { path: "/admin", element: <Admin />}
+          ]},
+        { element: <RequiredAuth allowedRole="USER"/>,
+          children: [
+            { path: "/user", element: <User />}
+          ]}
+        ]},
+    ]
+  }, 
+  { path: "unauthorized", element: <Unauthorized />},
+  { path: "register", element: <Register />},
+  { path: "login", element: <Login />},
+])
+
+export default router;
