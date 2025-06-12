efe4fb14ab635b6233ca229dc7fc8936dd767f02
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-12 19:06:14 +0800

    Add styling then import footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import AutoLogout from "./components/autoLogout"
 import Navbar from "./components/navbar"
 import { Outlet } from "react-router-dom"
+import Footer from "./components/footer"
 
 function App() {
 
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex flex-col font-poppins text-gray-800">
       <AutoLogout/>
       <Navbar />
-        <div className="px-14">
-            <Outlet />
-        </div>
+      <div className="py-5 bg-base-200 flex-grow">
+          <Outlet />
+      </div>
+      <Footer />
     </div>
   )
 }
