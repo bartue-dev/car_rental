cb9e36c27b65ec7172d4b8710ccfd68d0ecb6543
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-14 12:50:32 +0800

    Add create testimonial option to the navbar clickable profile

--- src/components/subComponents/profileStatus.jsx
+++ src/components/subComponents/profileStatus.jsx
@@ -40,7 +40,7 @@ function ProfileStatus() {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
-                <li><a className="text-base hover:bg-primary hover:text-white">Book now</a></li>
+                <li><Link to="/testimonials" className="text-base hover:bg-primary hover:text-white">Create testimonials</Link></li>
                 <li><a onClick={handleLogout} className="text-base hover:bg-primary hover:text-white">Logout</a></li>
               </ul>
             </div>
