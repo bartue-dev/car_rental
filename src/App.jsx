456e1bc9e4705e15f107b8b522cf6c2af02c6418
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-14 22:02:06 +0800

    Add conditional rendering. If user role is not ADMIN render this component. Which means if user role is ADMIN then do not render Navbar and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import AutoLogout from "./components/autoLogout"
 import Navbar from "./components/navbar"
 import { Outlet } from "react-router-dom"
 import Footer from "./components/footer"
+import useAuth from "./hooks/useAuth"
 
 function App() {
+  const { user } = useAuth();
 
   return (
     <div className="w-full h-screen flex flex-col font-poppins text-gray-800">
       <AutoLogout/>
-      <Navbar />
+
+      {user?.role !== "ADMIN" 
+       && <Navbar />}
+      {/* <Navbar /> */}
       <div className="py-5 bg-base-200 flex-grow">
           <Outlet />
       </div>
-      <Footer />
+
+      {user?.role !== "ADMIN" 
+       && <Footer />}
+      
     </div>
   )
 }
