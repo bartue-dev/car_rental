e688551ed93f4259b7732da4d5da6a4f99d8b5c8
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-14 22:08:39 +0800

    Parent component of sidebar component as the layout of whole sidebar or shouldi say dashboard

---
+++ src/components/sidebarLayout.jsx
@@ -0,0 +1,16 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/sidebar"
+
+export default function SidebarLayout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar/>
+      <main className="flex-1 flex flex-col min-h-screen relative">
+        <SidebarTrigger className="absolute -top-5"/>
+        <div className="flex-1 p-4 w-full h-full min-h-full">
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
+  )
+}
