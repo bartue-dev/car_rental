efae4c513e9ef970d388fb44fbde397d2612c0eb
Bartue <[bartbatiancila28@gmail.com]>
2025-06-25 23:25:53 +0800

    Check user role. If user is ADMIN then redirect it to the dashboard if not proceed to the home component

--- src/Pages/publicRoute/home.jsx
+++ src/Pages/publicRoute/home.jsx
@@ -1,7 +1,20 @@
 import Hero from "@/components/hero";
 import Vehicles from "@/components/vehicles";
+import useAuth from "@/hooks/useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const {user} = useAuth();
+  const navigate = useNavigate();
+
+  /* if user role is admin redirect to the dashboard */
+  useEffect(() => {
+    if (user?.role === "ADMIN") {
+      navigate("/dashboard")
+    }
+  },[navigate, user?.role])
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center mb-10">
