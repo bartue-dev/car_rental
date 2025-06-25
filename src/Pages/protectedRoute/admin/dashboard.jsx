d3605283d9735a07f2ea2fad37c788fbc1daf3ad
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-25 23:23:02 +0800

    Add styling

--- src/Pages/protectedRoute/admin/dashboard.jsx
+++ src/Pages/protectedRoute/admin/dashboard.jsx
@@ -2,9 +2,10 @@ import SidebarLayout from "@/components/sidebarLayout";
 import { Outlet } from "react-router-dom";
 
 function Dashboard() {
+  
   return (
     <SidebarLayout>
-      <div className="border">
+      <div>
         <Outlet/>
       </div>
     </SidebarLayout>
