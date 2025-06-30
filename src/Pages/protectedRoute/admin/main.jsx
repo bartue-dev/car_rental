88b8d5afda1c07475cf465756c662d7499cb6a14
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-30 18:40:55 +0800

    Import QuickActions component and remove the internal quick actions jsx

--- src/Pages/protectedRoute/admin/main.jsx
+++ src/Pages/protectedRoute/admin/main.jsx
@@ -1,6 +1,6 @@
-import RecentBookings from '@/components/recentBookingTable';
-import WeeklySummary from '@/components/weeklySummary';
-import { Car, ClipboardCheck, ArrowLeftToLine,  ArrowRightToLine  } from 'lucide-react';
+import QuickActions from '@/components/dashboard/quickActions';
+import RecentBookings from '@/components/dashboard/recentBookingTable';
+import WeeklySummary from '@/components/dashboard/weeklySummary';
 
 function Main() {
 
@@ -8,36 +8,7 @@ function Main() {
     <div >
       <WeeklySummary/>
       <RecentBookings/>
-     <div className="flex justify-between items-center mt-10">
-        <div className="border rounded-sm w-60 p-5 flex items-center gap-5 shadow-xs">
-          <Car size={45} strokeWidth={1}/>
-          <div>
-            <h1 className="text-sm">Total vehicles</h1>
-            <p className="text-xl">23</p>
-          </div>
-        </div>
-        <div className="border rounded-sm w-60 p-5 flex items-center gap-5 shadow-xs">
-          <ClipboardCheck size={30} strokeWidth={1}/>
-          <div>
-            <h1 className="text-sm">Rented</h1>
-            <p className="text-xl">15</p>
-          </div>
-        </div>
-        <div className="border rounded-sm w-60 p-5 flex items-center gap-5 shadow-xs">
-          <ArrowLeftToLine size={30} strokeWidth={1}/>
-          <div>
-            <h1 className="text-sm">To be return</h1>
-            <p className="text-xl">5</p>
-          </div>
-        </div>
-        <div className="border rounded-sm w-60 p-5 flex items-center gap-5 shadow-xs">
-          <ArrowRightToLine size={30} strokeWidth={1}/>
-          <div>
-            <h1 className="text-sm">To be pick</h1>
-            <p className="text-xl">2</p>
-          </div>
-        </div>
-      </div>
+      <QuickActions/>
     </div>
   )
 }
