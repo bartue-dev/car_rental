c259309b3db022d085cc12b839a6fb2260f95947
Bartue <[bartbatiancila28@gmail.com]>
2025-07-01 20:46:15 +0800

    Create a function that able to navigate to a certain path when click. Uses useNavigate from react-router-dom

--- src/components/dashboard/quickActions.jsx
+++ src/components/dashboard/quickActions.jsx
@@ -1,24 +1,47 @@
 import { ClipboardPlus, MessageCircle  } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 function QuickActions() {
+  const navigate = useNavigate();
+
+  const toBookings = () => {
+    navigate("/dashboard/admin/bookings")
+  }
+
+  const toVehicles = () => {
+    navigate("/dashboard/admin/vehicles")
+  }
+
+  const toTestimonials = () => {
+    navigate("/dashboard/admin/testimonials")
+  }
 
   return (
     <div className="mt-10">
       <h1 className="text-sm">Quick Actions</h1>
       <div className="flex justify-between items-center gap-5 mt-2">
-        <div className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer">
+        <div 
+          className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer"
+          onClick={toBookings}
+        >
           <ClipboardPlus size={35} strokeWidth={1}/>
           <div>
             <h1 className="text-sm">Add Bookings</h1>
           </div>
         </div>
-        <div className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer">
+        <div 
+          className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer"
+          onClick={toVehicles}
+        >
           <ClipboardPlus size={35} strokeWidth={1}/>
           <div>
             <h1 className="text-sm">Add Vehicle</h1>
           </div>
         </div>
-        <div className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer">
+        <div 
+          className="border rounded-sm w-full h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer"
+          onClick={toTestimonials}
+        >
           <MessageCircle size={30} strokeWidth={1}/>
           <div>
             <h1 className="text-sm">Review testimonials</h1>
