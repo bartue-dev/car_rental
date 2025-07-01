898b4b730501444101960bd09978862c5a16e698
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-01 20:28:38 +0800

    Import TestimonialsData component

--- src/Pages/protectedRoute/admin/admin-testimonials.jsx
+++ src/Pages/protectedRoute/admin/admin-testimonials.jsx
@@ -1,9 +1,10 @@
+import TestimonialsData from "@/components/dashboard/testimonialsData"
 
 function AdminTestimonials() {
 
   return (
     <div>
-      HELLO ADMIN TESTIMONIALS
+      <TestimonialsData/>
     </div>
   )
 }
