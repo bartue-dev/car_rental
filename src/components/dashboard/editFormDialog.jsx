f650929eb09923e5b000f6e0c0eb11d81b2552c1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-07 19:31:39 +0800

    Add a pickupDateTime value that uses date-fns libray to format the date and time then pass it to api request and also take the vehicleId then pass it to the api request. Import the PickupDateTime also add another Select component that render the vehicle data names

--- src/components/dashboard/editFormDialog.jsx
+++ src/components/dashboard/editFormDialog.jsx
@@ -17,12 +17,21 @@ import {
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import useAxiosPrivate from "@/hooks/useAxiosPrivate"
+import { format } from "date-fns";
 import { useState } from "react"
+import useAxiosPrivate from "@/hooks/useAxiosPrivate"
+import { PickupDateTime } from "../subComponents/dateTime";
 
-export function EditFormDialog({bookingId, firstName, lastName, address, phoneNumber, status, setBookings, setIsEditDialogOpen}) {
+
+export function EditFormDialog({setBookings, setIsEditDialogOpen, status, vehicles, ...booking}) {
   const axiosPrivate = useAxiosPrivate();
-  const [getStatus, setStatus] = useState("PENDING")
+  const [getStatus, setStatus] = useState(status || "PENDING")
+  const [pickupDate, setPickupDate] = useState(booking.pickupDateTime.split(", ")[0])
+  const [vehicleId, setVehicleId] = useState(booking.vehicleId)
+
+  // useEffect(() => {
+  //   console.log("pickupDate", pickupDate)
+  // },[])
 
   /* update booking function */
   const handleSaveDialog = async (e) => {
@@ -38,15 +47,19 @@ export function EditFormDialog({bookingId, firstName, lastName, address, phoneNu
       const phoneNumber = formData.get("phoneNumber");
       const status = getStatus
 
-      const response = await axiosPrivate.put(`/v1/booking-admin/${bookingId}`,
-        {firstName, lastName, address, phoneNumber, status}
+      
+      const formatedDate = format(new Date(pickupDate), "MM/dd/yyyy")
+      const pickupDateTime = `${formatedDate}, ${formData.get("pickupTime")}`
+
+      const response = await axiosPrivate.put(`/v1/booking-admin/${booking.bookingId}`,
+        {firstName, lastName, address, phoneNumber, pickupDateTime, status, vehicleId}
       )
 
       console.log("UPDATE BOOKING DETAILS: ", response);
 
       setBookings(prev => (
         prev.map((item) => {
-          return item.bookingId === bookingId ? {...item, ...response?.data?.data?.updatedBookingDetails} : item
+          return item.bookingId === booking.bookingId ? {...item, ...response?.data?.data?.updatedBookingDetails} : item
         })
       ))
 
@@ -57,27 +70,37 @@ export function EditFormDialog({bookingId, firstName, lastName, address, phoneNu
   }
 
   return (
-    <DialogContent className="sm:max-w-[425px]">
+    <DialogContent className="sm:max-w-[425px] font-poppins">
       <form onSubmit={handleSaveDialog} className="flex flex-col gap-5">
         <DialogHeader>
           <DialogTitle>Edit Booking Details</DialogTitle>
         </DialogHeader>
-        <div className="grid gap-4">
+        <div className="grid grid-cols-2 gap-4">
           <div className="grid gap-3">
             <Label htmlFor="firstname">Firstname</Label>
-            <Input id="firstname" name="firstName" defaultValue={firstName} />
+            <Input id="firstname" name="firstName" defaultValue={booking.firstName} />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="lastname">Lastname</Label>
-            <Input id="lastname" name="lastName" defaultValue={lastName} />
+            <Input id="lastname" name="lastName" defaultValue={booking.lastName} />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="address">Address</Label>
-            <Input id="address" name="address" defaultValue={address} />
+            <Input id="address" name="address" defaultValue={booking.address} />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="phoneNumber">Phone #</Label>
-            <Input id="phoneNumber" name="phoneNumber" defaultValue={phoneNumber} />
+            <Input id="phoneNumber" name="phoneNumber" defaultValue={booking.phoneNumber} />
+          </div>
+          <div className="col-span-2">
+            <fieldset className="border rounded-md p-3 border-gray-300 shadow-xs">
+              <legend className="text-sm font-semibold">Select Pickup</legend>
+              <PickupDateTime 
+                pickupDate={pickupDate} 
+                setPickupDate={setPickupDate}
+                pickupTime={booking.pickupDateTime.split(", ")[1]}
+              />
+            </fieldset>
           </div>
           <div className="grid gap-3">
             <Label htmlFor="status">Status</Label>
@@ -85,12 +108,28 @@ export function EditFormDialog({bookingId, firstName, lastName, address, phoneNu
               setStatus(value)
             }}>
               <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select Status" />
+                <SelectValue/>
               </SelectTrigger>
               <SelectContent>
                 <SelectItem value="PENDING">PENDING</SelectItem>
                 <SelectItem value="CONFIRM">CONFIRM</SelectItem>
                 <SelectItem value="COMPLETED">COMPLETED</SelectItem>
+                <SelectItem value="DECLINED">DECLINED</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="vehicle">Vehicles</Label>
+            <Select id="vehicle" defaultValue={vehicleId} onValueChange={(value) => {
+              setVehicleId(value)
+            }}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="-vehicles-"/>
+              </SelectTrigger>
+              <SelectContent>
+                {vehicles.map((vehicle) => (
+                  <SelectItem key={vehicle.vehicleId} value={vehicle.vehicleId}>{vehicle.name}</SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
