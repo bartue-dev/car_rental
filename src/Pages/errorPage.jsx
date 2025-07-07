04d575a6a2430ae0001a6b56b39b122fcac305c4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-07 19:20:02 +0800

    Rename class to className

--- src/Pages/errorPage.jsx
+++ src/Pages/errorPage.jsx
@@ -10,14 +10,14 @@ function ErrorPage() {
   const goBack = () => navigate(-1)
 
   return (
-   <section class="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
-    <div class="container flex flex-col items-center ">
-        <div class="flex flex-col gap-6 max-w-md text-center">
-            <h2 class="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
-                <span class="sr-only">{error.statusText || error.message}</span>404
+   <section className="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
+    <div className="container flex flex-col items-center ">
+        <div className="flex flex-col gap-6 max-w-md text-center">
+            <h2 className="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
+                <span className="sr-only">{error.statusText || error.message}</span>404
             </h2>
-            <p class="text-2xl md:text-3xl dark:text-gray-300">Sorry, we couldn't find this page.</p>
-            <button onClick={goBack} class="px-8 py-4 text-xl font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200 cursor-pointer">Back to home</button>
+            <p className="text-2xl md:text-3xl dark:text-gray-300">Sorry, we couldn't find this page.</p>
+            <button onClick={goBack} className="px-8 py-4 text-xl font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200 cursor-pointer">Back to home</button>
         </div>
     </div>
 </section>
