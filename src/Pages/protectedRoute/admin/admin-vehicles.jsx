158154942935643dbdd2ddbe0eea529e20ca8496
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-11 12:53:10 +0800

    Import useLocation from react-router-dom. Use it to retrieve the current url of the page. Then split the url and took the last element to have a conditional rendering if path is equal to add-vehicle then render the Outlet page of the vehicle page otherwise render the vehicles  page.

--- src/Pages/protectedRoute/admin/admin-vehicles.jsx
+++ src/Pages/protectedRoute/admin/admin-vehicles.jsx
@@ -1,23 +1,34 @@
 import Vehicles from "@/components/dashboard/vehiclesComponents/vehicles";
 import { ClipboardPlus } from 'lucide-react';
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 
 function AdminVehicles() {
+  const location = useLocation();
+
+  const path = location.pathname.split("/").pop();
 
   return(
     <div>
-       <div className="mb-5">
-        <h1 className="text-sm">Quick Actions</h1>
-        <div className="flex justify-between items-center gap-5 mt-2">
-          <div className="border rounded-sm w-60 h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer">
-            <ClipboardPlus size={35} strokeWidth={1}/>
-            <div>
-              <h1 className="text-sm">Add Vehicle</h1>
+      { path === "add-vehicle"
+        ? <Outlet/>
+        : <div>
+            <div className="mb-5">
+              <h1 className="text-sm">Quick Actions</h1>
+              <div className="flex justify-between items-center gap-5 mt-2">
+                <Link 
+                  to="/dashboard/admin/vehicles/add-vehicle" 
+                  className="border rounded-sm w-60 h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer"
+                >
+                  <ClipboardPlus size={35} strokeWidth={1}/>
+                  <div>
+                    <h1 className="text-sm">Add Vehicle</h1>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <Vehicles/>
+            <Vehicles/>
+          </div>}
     </div>
   )
 }
