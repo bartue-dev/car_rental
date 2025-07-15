2b453d5e4c10d3831c7414e11b3038eccb44679d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-15 23:12:42 +0800

    Reimport the AutoLogout component

--- src/Pages/persistLogin.jsx
+++ src/Pages/persistLogin.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
-import AutoLogout from "../components/autoLogout";
+import AutoLogout from "../components/common/autoLogout";
 
 //Persist login component that wrap all the protected route in routes
 function PersistLogin() {
