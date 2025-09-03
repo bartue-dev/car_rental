d70ae1f33eeba6bc927ce9bbf1f0407b293356f2
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-03 11:20:10 +0800

    Delete all files for fresh start

--- src/hooks/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../context/authProvider";
-
-//useAuth custom hook
-function useAuth() {
-  return useContext(AuthContext)
-}
-
-export default useAuth;
--- utils/randomColor.js
+++
@@ -1,6 +0,0 @@
-export const getRandomColor = () => {
-  const r = Math.floor(Math.random() * 200); // 0-199 instead of 0-255
-  const g = Math.floor(Math.random() * 200);
-  const b = Math.floor(Math.random() * 200);
-  return `rgb(${r}, ${g}, ${b})`;
-};
