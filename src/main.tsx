5e20300d742174c7fb68ea7ba63a5614fbaf67d1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-13 18:31:21 +0800

    Remove the AuthProvider from auth-provider. Replace it with Provider from redux. ALso use the persistGate from redux-persist for state persistent

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AuthProvider } from './context/auth-provider.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes.tsx'
+import { Provider } from "react-redux";
+import { store, persistor } from './store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}/>
-    </AuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}/>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
