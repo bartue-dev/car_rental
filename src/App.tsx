c1b6a83480ffd141253f36ce5bf3e943b49d44fc
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-14 15:09:34 +0800

    Import Navbar and Footer component. Only render if user role is not ADMIN

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router-dom"
+import { useAppSelector } from "./feature/hooks"
+import { userState } from "./feature/user/user-slice"
+import Navbar from "./components/common/navbar"
+import Footer from "./components/common/footer"
 
 function App() {
+  const user = useAppSelector(userState)
 
   return (
-    <div>
-      <Outlet/>
+    <div className="h-screen flex flex-col font-inter text-gray-800">
+
+      {user?.role !== "ADMIN"
+        && <Navbar />}
+
+       <div className="py-5 bg-base-200 flex-grow">
+          <Outlet />
+      </div>
+
+      {user?.role !== "ADMIN"
+        && <Footer />}
+
     </div>
   )
 }
