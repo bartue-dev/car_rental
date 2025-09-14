1f5198cd23733ad58c45eca3edad0cf6aac672b4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-14 11:01:50 +0800

    Implement a logout function using useLogout custom hook. Just for testing if the localStorage will clear a after

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,32 @@
 import { useAppSelector } from "@/feature/hooks"
 import { username as sliceUsername } from "@/feature/user/user-slice"
+import useLogout from "@/hooks/use-logout"
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const username = useAppSelector(sliceUsername)
+  const logout = useLogout()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+
+      navigate("/login")
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <div>
       <h1>{username}</h1>
+      <button
+        onClick={handleLogout}
+        className="cursor-pointer"
+      >
+        logout
+      </button>
     </div>
   )
 }
