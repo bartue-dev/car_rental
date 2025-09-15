043322e6251223209ea7529db20f70dac6aafe63
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-15 22:04:15 +0800

    Import QueryClient and QueryClientProvider from react-query the wrap the RouterProvider with QueryClientProvider to be able to use the methods from react-query

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,17 @@ import { router } from './routes.tsx'
 import { Provider } from "react-redux";
 import { store, persistor } from './store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router}/>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}/>
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   </StrictMode>,
