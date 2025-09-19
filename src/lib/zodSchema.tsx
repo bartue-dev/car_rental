d1e34f01429abdc62d9ce26087c172c5214a4ba3
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-19 23:36:38 +0800

    CReates a zodSchema where the shcemas store to be reusable and readble

---
+++ src/lib/zodSchema.tsx
@@ -0,0 +1,26 @@
+import { z } from "zod"
+
+export const BookingSchema = z.object({
+  firstName: z.string().min(1, "Firstname must not be empty").regex(/^[a-zA-Z]+$/, "Firstname must be characters only"),
+  lastName: z.string().min(1, "Lastname must not be empty").regex(/^[a-zA-Z]+$/, "Firstname must be characters only"),
+  address: z.string().min(1, "Address must bot be empty"),
+  phoneNumber: z.string().regex(/^(\+\d{12}|\d{11})$/, "Invalid Phone number"),
+  pickupDate: z.date("Invalid Date"),
+  pickupTime: z.string(),
+  pickupDateTime: z.string() 
+}).superRefine(({pickupDate}, ctx) => { //custom conditional validation
+  const today = new Date().setHours(0, 0, 0, 0);
+  const selectedDate = new Date(pickupDate).setHours(0, 0, 0, 0);
+
+  console.log(today)
+  console.log(selectedDate)
+  
+  if (selectedDate < today) {
+    console.log("Date must be today or later")
+    ctx.addIssue({
+      code: "custom",
+      message: "Date must be today or later",
+      path: ["pickupDate"]
+    })
+  }
+})
