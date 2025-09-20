dc4b8461194a0f3dcbc71ebcb00157b6dc1caf36
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-20 23:54:25 +0800

    Creates a Main component that renders DataSummary, RecentBookings and QuickActions components

---
+++ src/components/dashboard/main.tsx
@@ -0,0 +1,16 @@
+import DataSummary from "./data-summary";
+import RecentBookings from "./recent-bookings-table";
+import QuickActions from "./quick-actions";
+
+function Main() {
+
+  return (
+    <div >
+      <DataSummary/>
+      <RecentBookings/>
+      <QuickActions/>
+    </div>
+  )
+}
+
+export default Main;
