b4ffa497e6f551bcc897ebdcc8c280f9b1d1fce0
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-22 21:57:35 +0800

    Added \s to the regex validation so it would allow spaces and letters only. Also add the status as string and optional

--- src/lib/zodSchema.tsx
+++ src/lib/zodSchema.tsx
@@ -1,13 +1,14 @@
 import { z } from "zod"
 
 export const BookingSchema = z.object({
-  firstName: z.string().min(1, "Firstname must not be empty").regex(/^[a-zA-Z]+$/, "Firstname must be characters only"),
-  lastName: z.string().min(1, "Lastname must not be empty").regex(/^[a-zA-Z]+$/, "Firstname must be characters only"),
+  firstName: z.string().min(1, "Firstname must not be empty").regex(/^[a-zA-Z\s]+$/, "Firstname must be characters only"),
+  lastName: z.string().min(1, "Lastname must not be empty").regex(/^[a-zA-Z\s]+$/, "Firstname must be characters only"),
   address: z.string().min(1, "Address must bot be empty"),
   phoneNumber: z.string().regex(/^(\+\d{12}|\d{11})$/, "Invalid Phone number"),
   pickupDate: z.date("Invalid Date"),
   pickupTime: z.string(),
-  pickupDateTime: z.string() 
+  pickupDateTime: z.string(),
+  status: z.string().optional()
 }).superRefine(({pickupDate}, ctx) => { //custom conditional validation
   const today = new Date().setHours(0, 0, 0, 0);
   const selectedDate = new Date(pickupDate).setHours(0, 0, 0, 0);
