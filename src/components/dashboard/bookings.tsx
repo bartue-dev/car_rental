81746e6831f76577e6a21c2f504a4809b500d4da
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-22 22:11:56 +0800

    Bookings Component renders the booking table and quick action link.If add-booking is click and the last slash of path is /add-booking it will render the bookingForm instead

---
+++ src/components/dashboard/bookings.tsx
@@ -0,0 +1,30 @@
+import BookingTable from "./booking-table";
+import { Outlet, useLocation, Link } from "react-router-dom";
+import { ClipboardPlus } from 'lucide-react';
+
+export default function Bookings() {
+  const location = useLocation();
+
+  const addBookingPath = location.pathname.split("/").pop()
+
+  return (
+    <div>
+      {addBookingPath === "add-booking"
+        ? <Outlet />
+        :  <div>
+              <div className="mb-5">
+                <h1 className="text-sm">Quick Actions</h1>
+                <div className="flex justify-between items-center gap-5 mt-2">
+                  <Link to="/dashboard/admin/bookings/add-booking" className="border rounded-sm w-60 h-20 p-5 flex items-center gap-5 shadow-xs cursor-pointer bg-white">
+                    <ClipboardPlus size={35} strokeWidth={1}/>
+                    <div>
+                      <h1 className="text-sm">Add Bookings</h1>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <BookingTable/>
+          </div>}
+    </div>
+  )
+}
