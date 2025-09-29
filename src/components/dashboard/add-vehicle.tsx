0f0a787d4100833ba8224a4ed3322a9d2bd02d58
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-29 20:30:03 +0800

    CReate a add-vehicle component. A form that able to input a vehicleData and post it to the backend

---
+++ src/components/dashboard/add-vehicle.tsx
@@ -0,0 +1,18 @@
+import VehicleForm from './vehicle-form';
+import { ArrowLeftToLine } from 'lucide-react';
+import { useNavigate } from "react-router-dom";
+
+export default function AddVehicle() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="">
+      <ArrowLeftToLine
+        color="#4b5563" 
+        className="col-span-2 cursor-pointer mb-4"
+        onClick={() => navigate("/dashboard/admin/vehicles")}
+      />
+      <VehicleForm/>
+    </div>
+  )
+}
