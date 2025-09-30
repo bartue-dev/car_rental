ce0a5d9cdb4a3f8a36573c1b5c4490c101f0b57e
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-09-30 23:04:55 +0800

    Import AutoLogout component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useAppSelector } from "./feature/hooks"
 import { userState } from "./feature/user/user-slice"
 import Navbar from "./components/common/navbar"
 import Footer from "./components/common/footer"
+import AutoLogout from "./components/common/auto-logout"
 
 function App() {
   const user = useAppSelector(userState)
 
   return (
     <div className="h-screen flex flex-col font-inter text-gray-800">
+      <AutoLogout/>
 
       {user?.role !== "ADMIN"
         && <Navbar />}
